6a0fc7daca9658cdacb90ca86b46af9b3423e514
Alex Mwaura <alexmwaura36@gmail.com>
2022-08-18 18:19:19 +0300

    commit tree 6a3c90b55028cba5dab2bbc0d32838606b86454c

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
+    def name= (person_name)
+        @name = person_name
+    end
+    def name
+        @name
+    end
+    def job= (job_name)
+        @job = job_name
+    end
+    def job
+        @job
+    end
 
 end
+person=Person.new
+person.name = "travis scott"
+puts person.name
+person.job = "rapper"
+puts person.job
